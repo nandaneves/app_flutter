7be7333d7f35c493bc89a8a7a8c8269bcd80bc7b
FERNANDA MIRANDA NEVES <fernanda.2960@aluno.pr.senac.br>
2023-06-19 11:06:57 -0300

    yffy

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:app_lista_tarefas/paginas/paginas_list.dart';
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Pagina_lista(),
+    );
+  }
+}
+
---
+++ lib/paginas/paginas_list.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Pagina_lista extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child:Padding(
+          padding: const EdgeInsets.all(11),
+        child: TextField(),
+         ),
+      ),
+    );
+  }
+}
