75e670a8edeeaf11c1eec8d1949316e8fb37f17a
FERNANDA MIRANDA NEVES <fernanda.2960@aluno.pr.senac.br>
2023-06-29 10:09:17 -0300

    yffy

--- lib/paginas/paginas_list.dart
+++ lib/paginas/paginas_list.dart
@@ -4,10 +4,24 @@ class Pagina_lista extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child:Padding(
-          padding: const EdgeInsets.all(11),
-        child: TextField(),
-         ),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: TextField(
+            decoration: InputDecoration(
+                labelText: 'email',
+                hintText: 'exemplo@exemplo.com',
+                border: OutlineInputBorder(),
+                prefixText: "R\$",
+                suffixText: "Olá, mundo"),
+            obscureText: true,
+            obscuringCharacter: 'F',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+              color: Colors.blueAccent,
+            ),
+          ),
+        ),
       ),
     );
   }
